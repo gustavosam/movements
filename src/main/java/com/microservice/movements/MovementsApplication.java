84a0addaabc1e8d947161571a5e46b7baa894a46
Luis Gustavo Sebastian Sam <gustavosebastiansam@outlook.com>
2023-09-18 16:13:28 -0500

    El MS Movements se registra en el servidor Eureka

--- src/main/java/com/microservice/movements/MovementsApplication.java
+++ src/main/java/com/microservice/movements/MovementsApplication.java
@@ -2,10 +2,12 @@ package com.microservice.movements;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Esta es la clase principal de movimientos.
  * */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MovementsApplication {
 
